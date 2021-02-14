b4a3c807226a47da452989659e942cd0287b35a9
Rommel Liendo <cloudyrl1@gmail.com>
2021-02-14 00:06:43 -0400

    feat(first commit)

---
+++ contracts/SmartContract.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.6.4;
+
+contract SmartContract {
+
+struct Transaction { 
+    string receiver;
+    string sender;
+    string inventory1;
+    string inventory2;
+    string item;
+    uint amount;
+}
+
+mapping (string => Transaction) public transactionsMap;
+
+  function getTransaction(string memory _id) view public returns (string memory receiver , string memory sender , string memory inventory1, string memory inventory2 , string memory item , uint amount)
+  {
+    Transaction memory transaction = transactionsMap[_id];
+    receiver = transaction.receiver;
+    sender = transaction.sender;
+    inventory1= transaction.inventory1;
+    inventory2 = transaction.inventory2;
+    item = transaction.item;
+    amount = transaction.amount;
+  }
+
+  function addTransaction(string memory _id ,string memory _sender,string memory _receiver , string memory _inventory1, string memory _inventory2, string memory _item , uint _amount ) public {
+    Transaction memory transaction;
+    transaction.receiver = _receiver;
+    transaction.sender = _sender;
+    transaction.inventory1 = _inventory1;
+    transaction.inventory2 = _inventory2;
+    transaction.item = _item;
+    transaction.amount = _amount;
+    transactionsMap[_id] = transaction;
+  }
+}
---
+++ server.js
@@ -0,0 +1,49 @@
+const web3 = require('web3');
+const connection = new web3('HTTP://127.0.0.1:7545');
+const fs = require('fs');
+
+const contractAbi = JSON.parse(fs.readFileSync('./build/contracts/SmartContract.json','utf8'));
+const contract = new connection.eth.Contract(contractAbi.abi,'0x567f15cCa7c4bcb3bD23351D3052F257D6aA4275');
+
+// const init = async () => {
+//     try {
+//         const accounts = await connection.eth.getAccounts()
+//         console.log(accounts)
+//         const accountBalance = await connection.eth.getBalance('0xc21C028629e101bBD94B4Bbe260629910A50324d')
+//         console.log(accountBalance)
+
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// const deployContract = async () => {
+//     const contractAbi = JSON.parse(fs.readFileSync('SmartContract_sol_SmartContract.abi'));
+//     const contract = new connection.eth.Contract(contractAbi);
+//     const contractBytecode = fs.readFileSync('SmartContract_sol_SmartContract.bin').toString();
+//     const listOfPosts = ['A Beginner’s Guide to Ethereum','How Does Ethereum Work?','The Year in Ethereum','What is Ethereum 2.0?', 'Ethereum is a Dark Forest'];
+//     const deployedContract  = await contract.deploy({
+//         data: contractBytecode,
+//         arguments: [listOfPosts.map((name) =>
+//           connection.utils.asciiToHex(name))]
+//         }).send({
+//           from: '0xb5f6eC03D47080C8A050F5B8Eff1933C0ce2E9e9',
+//           gasPrice: connection.utils.toWei('0.00003', 'ether'),
+//           gas: 1500000
+//         })
+//     contract.options.address = deployedContract.options.address;
+//     console.log(deployedContract.options.address);
+// }
+
+const addTransaction = async () => {
+    const response = await contract.methods.addTransaction('1','Baron','Rommel','inventario 1','inventario 2','acetaminofen',200).send({from: '0xFAa11d69E6b8D2e98b616dB96c5dE5724Eaf0ff7',gas:3000000})
+    console.log(response)
+}
+
+const getTransaction = async () => {
+    const response = await contract.methods.getTransaction('1').call();
+}
+
+
+// addTransaction();
+// getTransaction();
