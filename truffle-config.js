fd783dc42076c6318f104d449f1fb56c9040eebf
Rommel Liendo <cloudyrl1@gmail.com>
2021-04-15 00:18:01 -0400

    feat(integration)

--- truffle-config.js
+++ truffle-config.js
@@ -10,7 +10,7 @@ module.exports = {
       host: "localhost",
       port: 7545,
       network_id: "5777",
-      from : "0xc9273aA2205F20a47CdDd0B7e7716dc3904a1748"
+      from : "0x38F64D180B5a2EbE549Cf4766605fc3f77F35B3b"
     }
   }
 }
